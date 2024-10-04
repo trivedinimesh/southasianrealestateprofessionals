64de1659a1e9b1934074a4f89fc56904ec2bfc4e
Nimesh Trivedi <trivedinimesh6@gmail.com>
2024-10-04 14:35:18 +0530

    nimesh push this data

--- app/Http/Controllers/FrontEnd/MembershipController.php
+++ app/Http/Controllers/FrontEnd/MembershipController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Plan;
 
 class MembershipController extends Controller
 {
     public function index()
     {
-        return view('frontend.membership.index');
+        // return view('frontend.membership.index');
+
+        $plans = Plan::all();
+        return view('frontend.membership.index')->with('plans', $plans);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionTableSeeder::class,
             SponsorSeeder::class,
+            PlanSeeder::class,
         ]);
     }
 }
